3c5a43b0a3b01b7e87c2e5edb48c7da038b73abb
Pepe Valdivia <pepe.valdivia.caballero@gmail.com>
2023-09-06 21:23:42 -0500

    login

--- app/src/main/java/pe/edu/ulima/pm20232/aulavirtual/screens/LoginScreen.kt
+++ app/src/main/java/pe/edu/ulima/pm20232/aulavirtual/screens/LoginScreen.kt
@@ -10,18 +10,21 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import pe.edu.ulima.pm20232.aulavirtual.R
-import pe.edu.ulima.pm20232.aulavirtual.ui.theme.Orange400
-import pe.edu.ulima.pm20232.aulavirtual.ui.theme.White400
+import pe.edu.ulima.pm20232.aulavirtual.ui.theme.*
 import androidx.compose.material.Text as Text1
 
 @Composable
@@ -34,12 +37,12 @@ fun ButtonWithIcon(
         onClick = { onClick() },
         modifier = Modifier
             .fillMaxWidth()
-            .height(50.dp),
+            .height(55.dp),
         colors = ButtonDefaults.buttonColors(
-            backgroundColor = Color.Blue, // Button background color
+            backgroundColor = Orange400, // Button background color
             contentColor = Color.White // Text and icon color
         ),
-        contentPadding = PaddingValues(start = 20.dp, end = 20.dp),
+        contentPadding = PaddingValues(start = 20.dp, end = 20.dp, top = 20.dp, bottom = 20.dp),
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically
@@ -47,7 +50,7 @@ fun ButtonWithIcon(
             Icon(
                 imageVector = icon,
                 contentDescription = null,
-                modifier = Modifier.size(24.dp)
+                modifier = Modifier.size(32.dp)
             )
             Spacer(modifier = Modifier.width(8.dp))
             Text1(text = text)
@@ -69,7 +72,9 @@ fun TextFieldWithLeadingIcon(
         modifier = Modifier
             .fillMaxWidth()
             //.border(1.dp, borderColor)
-            .padding(20.dp),
+            .padding(5.dp)
+            .background(color = Color.Transparent)
+            ,
         value = text,
         onValueChange = {
             onTextChanged(it)
@@ -79,9 +84,9 @@ fun TextFieldWithLeadingIcon(
         },
         singleLine = true,
         colors = TextFieldDefaults.textFieldColors(
-            // backgroundColor = Color.White,
-            focusedIndicatorColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent
+             backgroundColor = Color.White,
+            focusedIndicatorColor = Color.LightGray,
+            unfocusedIndicatorColor = Orange800
         ),
         leadingIcon = {
             Icon(
@@ -98,11 +103,7 @@ fun TextFieldWithLeadingIcon(
 }
 
 @Composable
-fun LoginScreen() {
-    val configuration = LocalConfiguration.current
-    val screenWidthDp = configuration.screenWidthDp
-    val screenHeightDp = configuration.screenHeightDp
-
+fun topScreen(){
     Column(modifier =Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
@@ -114,64 +115,68 @@ fun LoginScreen() {
         ) {
             val paddingPercentage = 60;
             val paddingValue = with(LocalDensity.current) {
-                (paddingPercentage * 0.07f * 16.dp.toPx()).dp
+                (paddingPercentage * 0.02f * 16.dp.toPx()).dp
             }
             Column(
-                modifier = Modifier.padding(top = paddingValue)
+                modifier = Modifier.padding(top = paddingValue),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Image(
                     painter = painterResource(id = R.drawable.ic_ulima), // Replace with your SVG resource ID
                     contentDescription = "Universidad de Lima",
                     modifier = Modifier.size(120.dp),
-                    colorFilter = ColorFilter.tint(if (isSystemInDarkTheme()) White400 else Orange400),
+                    colorFilter = ColorFilter.tint(White400),
+                )
+                Text1(
+                    text = "Gimnasio UL",
+                    textAlign = TextAlign.Center,
+                    color = Color.White,
+                    //fontSize = 40.sp,
+                    modifier =  Modifier.padding(top = 20.dp, bottom = 20.dp),
+                        style = MaterialTheme.typography.h4.copy(
+                        fontSize = 40.sp,
+                        fontFamily = FontFamily(Font(R.font.caslon_classico_sc_regular)),
+                        color = if (isSystemInDarkTheme()) White400 else Orange400 // Apply the custom text color here
+                    )
                 )
             }
         }
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(5f)
-                .padding(8.dp),
-            contentAlignment = Alignment.Center
-        ){
-            Column {
-
-                Text1(text = "Example", textAlign = TextAlign.End, color = Color.Blue, fontSize = 12.sp)
-            }
-        }
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f)
-                .padding(8.dp),
-            contentAlignment = Alignment.Center
-        ){
-            Column {
-
-                Text1(text = "Example", textAlign = TextAlign.End, color = Color.Blue, fontSize = 12.sp)
-            }
-        }
     }
-    Column(modifier =Modifier.fillMaxSize()) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = (screenHeightDp * 0.05).dp)
-        ) {
+}
+
+@Composable
+fun loginForm(screenWidthDp: Int, screenHeightDp: Int){
+    Box( // caja gris (light)
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = (screenHeightDp * 0.30).dp,)
+            .background(Gray1200),
+    ) {
+        Box(modifier = Modifier.padding(
+            start = (screenWidthDp * 0.125).dp,
+            top = (40.dp)
+        ),){
             Box(
                 modifier = Modifier
-                    .size((screenWidthDp * 0.75).dp) // Adjust the size as needed
-                    .align(Alignment.Center)
-                    .border(1.dp, Color.White)
-                    .padding(10.dp)
+                    .size(
+                        (screenWidthDp * 0.75).dp,
+                        (screenHeightDp * 0.35).dp
+                    ) // Adjust the size as needed
+                    //.border(1.dp, Gray800)
+                    .background(White400)
+                    .shadow(
+                        elevation = 5.dp,
+                        shape = MaterialTheme.shapes.medium,
+                        //color = Color.Gray
+                    )
+                    .padding(start = 20.dp, top = 30.dp, bottom = 20.dp, end = 20.dp),
             ) {
-                Column(){
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center
-                    ){
-                        Text1("Bienvenido al Sistema")
-                    }
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text1(text ="Bienvenido al Sistema", fontWeight = FontWeight.Bold, fontSize = 18.sp)
                     TextFieldWithLeadingIcon(
                         leadingIcon = Icons.Default.Person, // Replace with your desired icon
                         placeholder = "Usuario",
@@ -189,7 +194,9 @@ fun LoginScreen() {
                         }
                     )
                     Row(
-                        modifier = Modifier.fillMaxWidth().padding(20.dp),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 25.dp),
                         horizontalArrangement = Arrangement.Center,
                     ){
                         ButtonWithIcon("INGRESAR", Icons.Default.Person, {})
@@ -198,4 +205,30 @@ fun LoginScreen() {
             }
         }
     }
+}
+
+@Composable
+fun goToReset(){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        contentAlignment = Alignment.BottomCenter
+    ){
+        Row() {
+            Text1(text = "Olvidó su contraseña? ", textAlign = TextAlign.End, color = Gray800, fontSize = 16.sp)
+            Text1(text = "Cambiala Aquí", textAlign = TextAlign.End, color = Orange400, fontSize = 16.sp, fontWeight = FontWeight.Bold)
+        }
+    }
+}
+
+@Composable
+fun LoginScreen() {
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp
+    val screenHeightDp = configuration.screenHeightDp
+
+    topScreen()
+    loginForm(screenWidthDp, screenHeightDp)
+    goToReset()
 }
